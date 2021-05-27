fdc8619972666936c4157bf963bfc236b2544e6d
grochavieira <guirocha.hopeisaba@gmail.com>
2021-05-27 15:55:29 -0300

    removed some junk

--- force-app/main/default/staticresources/LA_404/404.js
+++
@@ -1,3 +0,0 @@
-const c404Controller = ['$scope', '$http', '$sce', '$route', '$location', '$timeout', '$routeParams', function (scope, http, $sce, $route, $location, $timeout, $routeParams) {
-    
-}];
